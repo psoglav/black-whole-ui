4e382e1f4c349f629e082f860b6c1cd14fddfeb7
Andrey Nevinny <psoglav.ih8u@gmail.com>
2020-12-27 03:33:19 +0600

    improved version of dev-tools connection

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,12 @@ import '@/services/registration'
 
 Vue.config.productionTip = false
 
+if (process.env.NODE_ENV === 'development') {
+  require('@vue/devtools').connect()
+}
+
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
